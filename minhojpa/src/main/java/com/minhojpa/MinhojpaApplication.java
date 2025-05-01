cab13700e95bede4c20032b2a31cf2ab71d36c82
rivermh <kmh000904@gmail.com>
2025-05-01 10:06:01 +0900

    first commit

---
+++ minhojpa/src/main/java/com/minhojpa/MinhojpaApplication.java
@@ -0,0 +1,14 @@
+package com.minhojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhojpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhojpaApplication.class, args);
+	}
+
+}
+ 
---
+++ minhojpa/src/test/java/com/minhojpa/MinhojpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhojpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
