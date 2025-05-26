17f77c371dab712e2ab49a052799ea84a04b4591
rivermh <kmh000904@gmail.com>
2025-05-26 16:59:05 +0900

    20250526 PM 4:59

--- minhojpa/src/main/java/com/minhojpa/entity/Member.java
+++ minhojpa/src/main/java/com/minhojpa/entity/Member.java
@@ -36,35 +36,5 @@ public class Member {
 	
 	// 기본 생성자 (JPA는 기본 생성자가 꼭 필요함)
 	public Member() {
-	}
-	
-	// getter, setter
-	public Long getId() {
-		return id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
+	}	
 }
