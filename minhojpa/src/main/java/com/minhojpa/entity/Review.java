95aeb689db2f55d042c955ff1efe2067eb66d28c
rivermh <kmh000904@gmail.com>
2025-06-28 23:44:43 +0900

    20250628

---
+++ minhojpa/src/main/java/com/minhojpa/entity/Review.java
@@ -0,0 +1,14 @@
+package com.minhojpa.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Review {
+
+}
