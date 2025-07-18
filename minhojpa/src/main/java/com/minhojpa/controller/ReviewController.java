31f633f149cdc77ce9a7989d134bd5b170d81c0f
rivermh <kmh000904@gmail.com>
2025-07-18 16:50:23 +0900

    20250718

--- minhojpa/src/main/java/com/minhojpa/controller/ReviewController.java
+++ minhojpa/src/main/java/com/minhojpa/controller/ReviewController.java
@@ -1,5 +1,20 @@
 package com.minhojpa.controller;
 
-public class ReviewController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.minhojpa.repository.MemberRepository;
+import com.minhojpa.repository.PlaceRepository;
+import com.minhojpa.service.ReviewService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/reviews")
+public class ReviewController {
+	private final ReviewService reviewService;
+	private final PlaceRepository placeRepository;
+	private MemberRepository memberRepository;
+	
 }
