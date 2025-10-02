bc20a995d6c8a36d110c43b865b92fbb555b26c5
rivermh <kmh000904@gmail.com>
2025-10-02 12:41:45 +0900

    20251002

--- minhojpa/src/main/java/com/minhojpa/entity/PostLike.java
+++ minhojpa/src/main/java/com/minhojpa/entity/PostLike.java
@@ -1,19 +1,22 @@
 package com.minhojpa.entity;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity
 @Getter
+@Setter
 @NoArgsConstructor
+//Lombok가 매개변수가 없는 기본 생성자를 자동으로 만들어준다
 @EqualsAndHashCode(of = {"member", "post"})
+//equal()랑 hashCode() 메서드를 자동으로 만들어준다. 즉 member와 post가 같다면 같은 좋아요로 취급
 public class PostLike {
 
 	@Id
@@ -21,9 +24,11 @@ public class PostLike {
 	private Long id;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
 	private Member member;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id", nullable = false)
 	private Post post;
 	
 	public PostLike(Member member, Post post) {
