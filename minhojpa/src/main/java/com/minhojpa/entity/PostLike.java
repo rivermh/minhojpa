bb527f832ab18f590278efa28dcb51cf4446ce04
rivermh <kmh000904@gmail.com>
2025-10-06 11:30:25 +0900

    20251006

--- minhojpa/src/main/java/com/minhojpa/entity/PostLike.java
+++ minhojpa/src/main/java/com/minhojpa/entity/PostLike.java
@@ -1,5 +1,6 @@
 package com.minhojpa.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +12,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
